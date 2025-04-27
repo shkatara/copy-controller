cdf70e8e855a4909c864ea45237fbf518b03d249
Shubham Katara <shubham.katara@trivago.com>
2025-04-27 17:03:27 +0200

    Select targetted configmaps to copy

--- main.go
+++ main.go
@@ -4,10 +4,14 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"log"
 	"path/filepath"
+	"time"
 
 	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	informer "k8s.io/client-go/informers"
+
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/clientcmd"
@@ -26,10 +30,15 @@ func NewConfigMap() *configmap {
 
 func (cm *configmap) Run(client *kubernetes.Clientset, ctx context.Context) {
 	// create a shared informer factory
-	factory := informer.NewSharedInformerFactory(client, 0)
+	//factory := informer.NewSharedInformerFactory(client, 0)
+	factory := informer.NewSharedInformerFactoryWithOptions(client, 0, informer.WithNamespace(v1.NamespaceAll),
+		informer.WithTweakListOptions(func(options *metav1.ListOptions) {
+			options.LabelSelector = "trivago.com/copy=true"
+		}))
 	// Create a ConfigMap informer
 	// This will create a ConfigMap informer that watches all namespaces and caches the ConfigMaps in memory.
 	cmInformer := factory.Core().V1().ConfigMaps().Informer()
+
 	// Add event handlers to the informer on which we should act.
 	cmInformer.AddEventHandler(cache.ResourceEventHandlerFuncs{
 		AddFunc: cm.AddFunc,
@@ -39,16 +48,17 @@ func (cm *configmap) Run(client *kubernetes.Clientset, ctx context.Context) {
 	// Wait for the caches to be synced before starting the controller
 	if !cache.WaitForCacheSync(ctx.Done(), cmInformer.HasSynced) {
 		panic("failed to sync caches")
+	} else {
+		log.Println("Caches synced")
 	}
 	// Wait for the context to be done
 	<-ctx.Done()
-
 }
 
 func (cm *configmap) AddFunc(obj interface{}) {
 	configMap := obj.(*v1.ConfigMap)
 	// Handle the add event
-	fmt.Println("added a configmap with name: ", configMap.Name)
+	log.Println("Added ConfigMap: ", configMap.Name)
 	// Add the configmap name to the map
 	cm.configMapNames[configMap.Name] = configMap.Name
 }
@@ -59,6 +69,16 @@ func (cm *configmap) ReadConfigMapNames() []string {
 		names = append(names, name)
 	}
 	return names
+}
+
+func (cm *configmap) PrintConfigMaps() {
+	for {
+		for _, name := range cm.ReadConfigMapNames() {
+			log.Println("ConfigMap: ", name)
+		}
+		time.Sleep(5 * time.Second)
+	}
+
 }
 
 func main() {
@@ -85,7 +105,7 @@ func main() {
 
 	cm := NewConfigMap()
 	fmt.Println("Starting ConfigMap controller")
-	go cm.ReadConfigMapNames()
+	go cm.PrintConfigMaps()
 	cm.Run(clientset, ctx)
 
 }
